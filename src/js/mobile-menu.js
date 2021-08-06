3cbc7df4635380e83ba0d337387eabed1368b188
mandarinka99 <alina.oksentiuk@gmail.com>
2021-08-07 02:25:40 +0300

    header-mobile

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,6 +3,8 @@
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
   const mobileBtnClose = document.querySelector("[data-menu-close]");
+  const tabletFilter = document.querySelector("[data-filter]");
+  const tabletFilterBtn = document.querySelector("[data-filter-btn]");
 
   menuBtnRef.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
@@ -11,4 +13,9 @@
   mobileBtnClose.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
   });
+
+  tabletFilterBtn.addEventListener("click", () => {
+    tabletFilter.classList.toggle("is-open");
+  });
+
 })();
