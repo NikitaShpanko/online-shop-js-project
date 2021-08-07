90d2679c6cb3025d0538f456e467d06d6e407368
Nikita Shpanko <shpancore@gmail.com>
2021-08-07 04:11:23 +0300

    класс Render теперь весьма обучаемый

--- src/js/render.js
+++ src/js/render.js
@@ -1,12 +1,30 @@
-import cardTpl from '../templates/card.hbs';
-import categoriesTpl from '../templates/categories.hbs';
-
 import * as API from './api.js';
 import config from '../config.json';
 
-/** Класс Render прорисовывает основной контент на нашей странице. */
+/**
+ * Класс Render прорисовывает данные API в выбранном DOM элементе.
+ *
+ * То есть буквально: на входе адрес GET-запроса, на выходе -
+ * замена `innerHTML` в заданном
+ */
 export default class Render {
+  /**
+   * Шаблон ошибки, по умолчанию выдаёт JSON.
+   *
+   * Это статическое свойство для всех экземпляров класса,
+   * у которых оно явно не переназначено.
+   *
+   * @param {{error: number, errorText: string}} err
+   * @returns {string}
+   */
+  static errorTemplate = err => JSON.stringify(err);
+
+  /**
+   * Родительский элемент, HTML которого и будет перезаписываться.
+   * Задаётся и проверяется в конструкторе.
+   */
   #parent;
+
   /**
    * Приватная функция, возвращающая "хвост" текущего адреса страницы,
    * независимо от того, находится ли она на ГитХабе или на наших локальных
@@ -14,18 +32,75 @@ export default class Render {
    * @type {() => string}
    */
   #linkTail;
+
   /**
    * Приватная функция, изменяющая строку истории браузера независимо от того,
    * находится ли сайт на ГитХабе или на наших локальных серверах.
    * @type {function (string)}
    */
   #changeHistory;
+
   /**
-   * @param {Element | string} parent элемент, в котором осуществляется прорисовка
-   * @param {boolean} drawOnLoad нужно ли выполнять прорисовку
-   * автоматически при загрузке страницы (по умолчанию `true`)
+   * Преобразование данных перед подстановкой в шаблон.
+   *
+   * По умолчанию ничего не делает, просто передаёт дальше.
+   *
+   * Переопределите эту функцию в экземпляре или наследнике класса.
+   * @param {object} data
+   * @returns {object}
    */
-  constructor(parent = document, drawOnLoad = true) {
+  dataTransform = data => data;
+
+  /**
+   * Шаблон, т. е. преобразование данных в HTML-код.
+   *
+   * По умолчанию возвращает не HTML, а JSON, просто потому, что так проще.
+   *
+   * Переопределите эту функцию в экземпляре или наследнике класса.
+   *
+   * Можно просто присвоить переменной `template` функцию-шаблон (не вызов
+   * функции, а именно саму функцию).
+   * @param {object} data
+   * @returns {string}
+   */
+  template = data => JSON.stringify(data);
+
+  /**
+   * Шаблон ошибки для данного экземпляра класса.
+   *
+   * По умолчанию равен функции, заданной в статическом
+   * свойстве `Render.errorTemplate`.
+   * @param {{error: number, errorText: string}} err
+   * @returns {string}
+   */
+  errorTemplate = Render.errorTemplate;
+
+  /**
+   * Позволено ли этому экземпляру класса изменять адрес страницы,
+   * вписывая в неё запрос на API-сервер?
+   */
+  changeLink = false;
+
+  /**
+   * Позволено ли этому экземпляру класса изменять адрес,
+   * когда пользователь заходит на основную страницу (имитация
+   * переадресации)?
+   */
+  changeLinkOnRoot = false;
+
+  /**
+   * Конструктор создаёт экземпляр класса с достаточно примитивным поведением:
+   * выдающий в DOM-элемент JSON, пришедший с сервера. Без всякого форматирования.
+   *
+   * Но поведение экземпляра можно и нужно изменять, переопределяя его функции
+   * (точнее, свойства-ссылки на функции), а именно `dataTransform` и `template`.
+   *
+   * Также, если это необходимо, можно включить изменение адреса без перезагрузки
+   * страницы, с помощью свойств `changeLink` и `changeLinkOnRoot`.
+   *
+   * @param {Element | string} parent элемент, в котором осуществляется прорисовка
+   */
+  constructor(parent = document) {
     if (typeof parent === 'string') {
       parent = document.querySelector(parent);
     }
@@ -45,29 +120,59 @@ export default class Render {
       const rootUrl = link.slice(0, cutAt);
       this.#changeHistory = tail => history.pushState(null, null, rootUrl + tail);
     }
-    if (drawOnLoad) this.drawLink();
   }
   /**
    * Основной метод данного класса.
    *
-   * Принимает решение, что рисовать, исходя из указанного адреса.
+   * Отправляет GET-запрос по заданному адресу, обрабатывает ответ, как предписано
+   * функцией `dataTransform` и выдаёт результат в DOM по шаблону `template`.
+   *
+   * Умеет по умолчанию адреса работать с текущим адресом страницы
+   * и сам может изменять его без перезагрузки, если ему дадут на это
+   * полномочия свойствами `changeLink` и `changeLinkOnRoot`.
    *
    * @param {string} link  должен начинаться со слэша, по умолчанию -
    * "хвост" текущего адреса страницы.
    */
-  async drawLink(link = this.#linkTail()) {
-    let changeHistory = true;
-    if (link === '/' || link === '') {
+  async render(link = this.#linkTail()) {
+    let changeLink = this.changeLink;
+    if (link === '/' || link === '/index.html') {
       link = config.defaultServerLink;
-      changeHistory = false;
+      changeLink = this.changeLinkOnRoot;
     }
+    if (changeLink) this.#changeHistory(link);
+
     try {
       const data = await API.request(link);
-      this.#parent.innerHTML = cardTpl(Object.values(data)[0]);
-      //this.#parent.innerText = JSON.stringify(Object.values(data)[0]);
-      if (changeHistory) this.#changeHistory(link);
+      if (data.hasOwnProperty('error')) throw { error: data.error, errorText: data.errorText };
+      this.#parent.innerHTML = this.template(this.dataTransform(data));
     } catch (err) {
-      this.#parent.innerText = err;
+      this.#parent.innerHTML = this.errorTemplate(err);
     }
+  }
+
+  /**
+   * Метод похож на `render`, но более примитивен: не умеет работать с
+   * адресом страницы. Да ему это и не нужно, ведь его предназначение -
+   * помочь нам реализовать функционал "Загрузить ещё".
+   *
+   * @param {string} link должен начинаться со слэша, на сей раз этот
+   * параметр обязателен.
+   */
+  async append(link) {
+    try {
+      const data = await API.request(link);
+      if (data.hasOwnProperty('error')) throw { error: data.error, errorText: data.errorText };
+      this.#parent.insertAdjacentHTML('beforeend', this.template(this.dataTransform(data)));
+    } catch (err) {
+      this.#parent.insertAdjacentHTML('beforeend', this.errorTemplate(err));
+    }
+  }
+
+  /**
+   * Достучаться до родительского элемента можно здесь.
+   */
+  get parent() {
+    return this.#parent;
   }
 }
