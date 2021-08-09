94de9fb4ed9a8433b6e173e3bebe46b47be4505a
IgnatevD <ignatevds@gmail.com>
2021-08-09 12:08:50 +0300

    исправлена ошибка клика на кнопки

--- src/js/cardButton.js
+++ src/js/cardButton.js
@@ -2,7 +2,7 @@ const allCategorys = document.querySelector('body');
 
 allCategorys.addEventListener('click', (e) => {
     const buttonClick = e.target.closest('button');
-    if (buttonClick.nodeName !== 'BUTTON') return false;
+    if (buttonClick?.nodeName !== 'BUTTON') return false;
 
     if (buttonClick.classList.contains('icon-heart-white')) {
 
