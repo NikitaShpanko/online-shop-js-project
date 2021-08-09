00e3fa4d4a42c0dd74f2411cef3a3d4d5a710f93
IgnatevD <ignatevds@gmail.com>
2021-08-10 00:49:43 +0300

    Открытие карты товара  по кнопке

--- src/js/cardButton.js
+++ src/js/cardButton.js
@@ -1,24 +1,39 @@
 import modalCard from '../templates/modal-card.hbs';
+import allCardCategoryTpl from '../templates/categories-allcard.hbs';
 import { openModal, closeModal } from './modal-control';
 import store from '../lib/store';
 import * as API from '../lib/api';
 
 
 const allCategorys = document.querySelector('body');
+const mainNode = document.querySelector('#root');
 
 allCategorys.addEventListener('click', (e) => {
     const buttonClick = e.target.closest('button');
-    
-    if (buttonClick?.nodeName !== 'BUTTON') return false;
+    const modalCard = e.target.classList.contains('card__img');
+    const allCardCategory = e.target.classList.contains('categories__titel--link');
 
-    if (buttonClick.classList.contains('icon-heart-white') ) {
-        buttonClick.classList.toggle('isFavorites');    
-    }
-    else if (buttonClick.classList.contains('icon-fullscreen')) {
-        openModalCard();
-        console.log(store.categories);
-    }
+    // console.log(store.categories);
+    
+    if (buttonClick?.nodeName === 'BUTTON') {
+        if (buttonClick.classList.contains('icon-heart-white'))
+            buttonClick.classList.toggle('isFavorites')
+        else if (buttonClick.classList.contains('icon-fullscreen'))
+            openModalCard();
+    };
+
+    if (modalCard) openModalCard();
+    
+    if (allCardCategory) { mainNode.innerHTML = allCardCategoryTpl ()};
+
 });
+
+function buttonClickKard() {
+    if (buttonClick.classList.contains('icon-heart-white')) { buttonClick.classList.toggle('isFavorites') }
+    else if (buttonClick.classList.contains('icon-fullscreen')) { openModalCard() };
+}
+        
+
 
 function openModalCard() {
         openModal(modalCard);
