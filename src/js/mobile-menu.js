f6b2ce160af6db89376387c4fa6bf0740ece6b6d
mandarinka99 <alina.oksentiuk@gmail.com>
2021-08-09 21:16:05 +0300

    clear filter

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,26 +1,47 @@
-  
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
-  const mobileBtnClose = document.querySelector("[data-menu-close]");
-  const mobileFilterBtn = document.querySelector("[data-mobile-filter-btn]");
-  const tabletFilter = document.querySelector("[data-tab-filter]");
-  const tabletFilterBtn = document.querySelector("[data-tab-filter-btn]");
+import store from '../lib/store';
+import * as API from '../lib/api';
 
-  menuBtnRef.addEventListener("click", () => {
-    mobileMenuRef.classList.toggle("is-open");
-  });
+const menuBtnRef = document.querySelector("[data-menu-button]");
+const mobileMenuRef = document.querySelector("[data-menu]");
+const mobileBtnClose = document.querySelector("[data-menu-close]");
+const mobileFilterBtn = document.querySelector("[data-mobile-filter-btn]");
+const tabletFilter = document.querySelector("[data-tab-filter]");
+const tabletFilterBtn = document.querySelector("[data-tab-filter-btn]");
+const clearBtnsRef = document.querySelectorAll(".js-clear");
+const headerInputRef = document.querySelector(".js-input-search");
 
-  mobileBtnClose.addEventListener("click", () => {
-    mobileMenuRef.classList.toggle("is-open");
-  });
+menuBtnRef.addEventListener("click", () => {
+  mobileMenuRef.classList.toggle("is-open");
+});
 
-  mobileFilterBtn.addEventListener("click", () => {
-    document.querySelector("[data-mobile-filter]")
-      .classList.toggle("is-open-filter");
-  });
+mobileBtnClose.addEventListener("click", () => {
+  mobileMenuRef.classList.toggle("is-open");
+});
 
-  tabletFilterBtn.addEventListener("click", () => {
-    tabletFilter.classList.toggle("is-open-filter");
-  });
+mobileFilterBtn.addEventListener("click", () => {
+  document.querySelector("[data-mobile-filter]")
+    .classList.toggle("is-open-filter");
+});
+
+tabletFilterBtn.addEventListener("click", () => {
+  tabletFilter.classList.toggle("is-open-filter");
+});
+
+clearBtnsRef[0].addEventListener("click", () => {
+  store.setQuery(null);
+});
+
+clearBtnsRef[1].addEventListener("click", () => {
+  store.setQuery(null);
+});
+
+headerInputRef.addEventListener('input', e => {
+ const search = e.currentTarget.value;
+    if (search && search.length) {
+      store.setQuery({ search })
+    } else {
+      store.setQuery(null)
+    }
+})
 
   
