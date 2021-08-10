f4224a00dc490e2fff77221acdf7ebbe553da126
IgnatevD <ignatevds@gmail.com>
2021-08-10 18:02:35 +0300

    Собития на кнопки

--- src/js/cardButton.js
+++ src/js/cardButton.js
@@ -9,6 +9,7 @@ const bodyNode = document.querySelector('body');
 bodyNode.addEventListener('click', (e) => {
     const buttonClick = e.target.closest('button');
     const cardId = e.target.closest('.card__all--content');
+    e.preventDefault();
     
     if (buttonClick?.nodeName === 'BUTTON') {
         if (buttonClick.classList.contains('icon-heart-white')) {
@@ -17,9 +18,16 @@ bodyNode.addEventListener('click', (e) => {
         }
             
         else if (buttonClick.classList.contains('icon-fullscreen'))
-            openModalCard(cardId.dataset.id)
+            openModalCard(cardId.dataset.id);
         else if (buttonClick.classList.contains('load__more--button'))
             console.log("Загрузка следующей страницы ");
+        else if (buttonClick.classList.contains('button__arow--left'))
+            console.log("Товар слева");
+        else if (buttonClick.classList.contains('button__arow-right'))
+            console.log("Товар справа");
+        else if (buttonClick.classList.contains('categories__titel--allCard'))
+            console.log("Загружается шаблон со всеми карточками");
+        
     };
 
     if (cardId && buttonClick?.nodeName !== 'BUTTON') openModalCard(cardId.dataset.id);
