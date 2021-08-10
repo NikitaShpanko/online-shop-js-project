5610663b1eb2f572ee9068d1afabe1cc8d998818
IgnatevD <ignatevds@gmail.com>
2021-08-10 16:44:41 +0300

    Добавлены стиоли для карточек

--- src/js/cardButton.js
+++ src/js/cardButton.js
@@ -4,23 +4,27 @@ import { openModal, closeModal } from './modal-control';
 import store from '../lib/store';
 import * as API from '../lib/api';
 
-
 const bodyNode = document.querySelector('body');
-const mainNode = document.querySelector('#root');
 
-mainNode.addEventListener('click', (e) => {
+bodyNode.addEventListener('click', (e) => {
     const buttonClick = e.target.closest('button');
     const cardId = e.target.closest('.card__all--content');
     
     if (buttonClick?.nodeName === 'BUTTON') {
-        if (buttonClick.classList.contains('icon-heart-white'))
-            buttonClick.classList.toggle('isFavorites')
+        if (buttonClick.classList.contains('icon-heart-white')) {
+            buttonClick.classList.toggle('isFavorites');
+            console.log("добавить товар в избранное");
+        }
+            
         else if (buttonClick.classList.contains('icon-fullscreen'))
-            openModalCard(cardId.dataset.id);
+            openModalCard(cardId.dataset.id)
+        else if (buttonClick.classList.contains('load__more--button'))
+            console.log("Загрузка следующей страницы ");
     };
 
     if (cardId && buttonClick?.nodeName !== 'BUTTON') openModalCard(cardId.dataset.id);
 });
+
  
 function openModalCard(id) {
     openModal(modalCard());
