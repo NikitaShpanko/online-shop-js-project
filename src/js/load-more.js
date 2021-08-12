aac0e937e1268f81d0201a04bdc7ed6508b3cf92
mandarinka99 <alina.oksentiuk@gmail.com>
2021-08-13 01:54:37 +0300

    show-all-categories

--- src/js/load-more.js
+++ src/js/load-more.js
@@ -7,4 +7,7 @@ const loadMoreRef = document.querySelector('.js-load-more');
 loadMoreRef.addEventListener('click', e => {
   store.setQuery({ page: store.query && store.query.page ? store.query.page + 1 : 2 });
   console.log('query', store.query);
+  if (store.query.page >= 3) {
+    loadMoreRef.style.opacity = `0.5`;
+  }
 });
