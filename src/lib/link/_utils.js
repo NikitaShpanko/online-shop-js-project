7b0a54a97ff98629d4bf7a190ef15186e9381b6f
Nikita Shpanko <shpancore@gmail.com>
2021-08-13 11:11:56 +0300

    работает вёрстка для одной категории

--- src/lib/link/_utils.js
+++ src/lib/link/_utils.js
@@ -5,6 +5,8 @@ import cardTpl from '../../templates/card.hbs';
 import categoriesTpl from '../../templates/categories.hbs';
 import searchCardTpl from '../../templates/search-allcard.hbs';
 
+import categoryCardTpl from '../../templates/category-cards.hbs';
+
 function russify(name) {
   const rusName = store.rusCategoryNames[name];
   return rusName ? rusName : name;
@@ -18,7 +20,7 @@ function russify(name) {
 function getCatReady(name, cardList, linkBefore = '') {
   const category = {};
   category.name = russify(name);
-  category.card = cardTpl(cardList);
+  category.card = API.Card.tpl(cardList); //cardTpl(cardList);
   if (linkBefore) {
     category.link = linkBefore + category.name;
   }
@@ -44,6 +46,8 @@ export function renderData(data, filterString, linkBefore, clear = true) {
   const catList = data?.filter(filterString);
   if (!catList) return;
 
+  API.Card.tpl = cardTpl;
+
   catList.forEach(cat => {
     if (linkBefore) cat.link = linkBefore + cat.name;
     cat.name = russify(cat.name);
@@ -62,6 +66,8 @@ export function renderData(data, filterString, linkBefore, clear = true) {
  * @param {string} filterString
  */
 export function renderCategory(category, filterString, linkBefore) {
+  API.Card.tpl = categoryCardTpl;
+  console.log(getCatReady(category.name, category?.filter(filterString), linkBefore));
   document.querySelector('#root').innerHTML = searchCardTpl(
     getCatReady(category.name, category?.filter(filterString), linkBefore),
   );
