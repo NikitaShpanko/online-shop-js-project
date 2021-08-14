6cb57ff29daaa330c366c3335213048e79237a91
IgnatevD <ignatevds@gmail.com>
2021-08-15 01:34:07 +0300

    испарвлены стили ссылок на кнопках хедера

--- src/js/cardButton.js
+++ src/js/cardButton.js
@@ -21,7 +21,10 @@ bodyNode.addEventListener('click', e => {
     if (buttonClick.classList.contains('icon-heart-white')) {
       if (!localStorage.accessToken) return openModal(authorizationFormTpl());
       const getCardId = cardId.dataset.id;
-      console.log(card);
+      const data = store.products.getCard(getCardId);
+
+      console.log(data);
+
       buttonClick.classList.toggle('isFavorites');
       if (buttonClick.classList.contains('isFavorites')) postIsFavoritesCard(getCardId);
       else deleteIsFavoritesCard(getCardId);
