9155b43f1ef6fe65210f07b5b3f60919a6316525
Nikita Shpanko <shpancore@gmail.com>
2021-08-16 15:22:56 +0300

    та заработай уже, ну!

--- src/js/404.js
+++ src/js/404.js
@@ -1,11 +1,3 @@
-// import { goTo } from '../lib/link';
-console.log('HEY!');
-
-//const ourHref = location.href;
 location.href =
   'https://nikitashpanko.github.io/online-shop-js-project/index.html?redir=' +
   encodeURI(location.href);
-// document.addEventListener('load', () => {
-//   Link.goTo(ourHref);
-// });
-//Link.goTo(ourHref);
