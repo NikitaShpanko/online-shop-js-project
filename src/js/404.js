b7b0c5ce78acf7684556500a2e4fe4d3667c72a5
Nikita Shpanko <shpancore@gmail.com>
2021-08-16 15:21:26 +0300

    бесконечный редирект не пройдёт

--- src/js/404.js
+++ src/js/404.js
@@ -2,7 +2,9 @@
 console.log('HEY!');
 
 //const ourHref = location.href;
-location.href = './index.html?redir=' + encodeURI(location.href);
+location.href =
+  'https://nikitashpanko.github.io/online-shop-js-project/index.html?redir=' +
+  encodeURI(location.href);
 // document.addEventListener('load', () => {
 //   Link.goTo(ourHref);
 // });
